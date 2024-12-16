5b4b94cf6cb7c85aa12dc98592be1bcba6b993db
Daniyal-ali-code <daniyalaliwork24@gmail.com>
2024-12-16 23:31:14 +0500

    Daniyal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
+// search bar
+const data = [
+  "Chicken",
+  "Pineapple",
+  "Strawberry",
+  "Grapes",
+  "Mango",
+  "Peach"
+];
+
+function searchFunction() {
+  const searchTerm = document.getElementById("search-bar").value.toLowerCase();
+  const resultsDiv = document.getElementById("search-results");
+
+  resultsDiv.innerHTML = "";  // Clear previous results
+
+  // Filter the data array based on the search term
+  const filteredResults = data.filter(item => item.toLowerCase().includes(searchTerm));
+
+  // Display filtered results
+  if (filteredResults.length > 0) {
+      filteredResults.forEach(item => {
+          const resultItem = document.createElement("div");
+          resultItem.textContent = item;
+          resultsDiv.appendChild(resultItem);
+      });
+  } else {
+      resultsDiv.innerHTML = "No results found.";
+  }
+}
+// search bar end.
+
 const getElement = (selector) => {
   const element = document.querySelector(selector)
 
